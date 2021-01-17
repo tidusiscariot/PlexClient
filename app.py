310ef4be73d8523922f89bc71a55442e731420af
tidusiscariot <tidusiscariot@yahoo.com>
2021-01-17 10:00:47 -0500

    use group for Hue manipulation

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 # Small app to forward Plex webhooks to Hue bulbs.
+#
+# Hue API information https://developers.meethue.com/develop/hue-api/groupds-api/
+# Plex Webhooks information https://support.plex.tv/articles/115002267687-webhooks/
 import json
 import requests
 from flask import Flask, request
 import logging
-import os
 from logutils import logformat as lf
 
 # Create the app
 app = Flask(__name__)
 
 # Setup logging
-# logFile = os.path.relpath('.') + '/logs/debug.log'
-logFile = 'C:/hosted_python_apps/logs/PlexClient/debug.log'
-logging.basicConfig(filename=logFile, format='%(asctime)s %(message)s', level=logging.INFO)
+infoLogFile = 'C:/hosted_python_apps/logs/PlexClient/debug.log'
+errorLogFile = 'C:/hosted_python_apps/logs/PlexClient/error.log'
+logging.basicConfig(filename=infoLogFile, format='%(asctime)s %(message)s', level=logging.INFO)
+logging.basicConfig(filename=errorLogFile, level=logging.ERROR)
+
+local_phone_uuid = '013b038c87a5cd67-com-plexapp-android'
+urlGroup = 'http://192.168.1.95/api/rmnxM5hTeDs-3Oyot-zfpeC6bnd4Nog0sSkbRkJe/groups/1/action'
 
 
 # General GET
@@ -24,10 +30,6 @@ def hello():
 # Main home route that Plex posts webhooks to
 @app.route('/', methods=['POST'])
 def ingestPlexWebhook():
-    urlBulb1 = 'http://192.168.1.95/api/rmnxM5hTeDs-3Oyot-zfpeC6bnd4Nog0sSkbRkJe/lights/1/state'
-    urlBulb2 = 'http://192.168.1.95/api/rmnxM5hTeDs-3Oyot-zfpeC6bnd4Nog0sSkbRkJe/lights/2/state'
-    urlBulb3 = 'http://192.168.1.95/api/rmnxM5hTeDs-3Oyot-zfpeC6bnd4Nog0sSkbRkJe/lights/3/state'
-
     # Encode to a useful json obj
     jsonReq = json.loads(request.values.get('payload'))
 
@@ -45,8 +47,11 @@ def ingestPlexWebhook():
         logging.info(lf.formatLog(player, metaData))
         return ''
     # Exit if not my phone uuid
-    elif not player.get('uuid') == '013b038c87a5cd67-com-plexapp-android':
+    elif not player.get('uuid') == local_phone_uuid:
         return ''
+
+    # Probably don't need to always log local plays
+    logging.info(lf.formatLog(player, metaData))
 
     # Lot more to do if we're local and on my phone
     # Only change the bulbs for movies
@@ -55,15 +60,11 @@ def ingestPlexWebhook():
 
     # Only act on play/resume/pause/stop
     if event == 'media.play' or event == 'media.resume':
-        requests.put(urlBulb1, json={'on': True, 'bri': 30})
-        requests.put(urlBulb2, json={'on': True, 'bri': 30})
-        requests.put(urlBulb3, json={'on': True, 'bri': 30})
-        testing
+        # {"on":true,"bri":254, "hue":39392}
+        requests.put(urlGroup, json={'on': True, 'bri': 30})
 
     elif event == 'media.stop' or event == 'media.pause':
-        requests.put(urlBulb1, json={'on': True, 'bri': 254})
-        requests.put(urlBulb2, json={'on': True, 'bri': 254})
-        requests.put(urlBulb3, json={'on': True, 'bri': 254})
+        requests.put(urlGroup, json={'on': True, 'bri': 254})
 
     # Required
     return ''
